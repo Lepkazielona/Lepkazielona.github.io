e5fe05ed862b6722aff3114ad6f58a147e4f2da5
Lepka <lamaandsp@gmail.com>
2022-10-09 22:42:35 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,32 @@
+import './style.css'
+
+import * as THREE from 'three'
+
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio/3);
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshLambertMaterial( {color: 0xffffff} );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+var light = new THREE.AmbientLight(0x404040);
+scene.add(light);
+
+function animate(){
+  requestAnimationFrame(animate)
+  cube.rotation.x += 0.01
+  cube.rotation.y += 0.01
+  renderer.render(scene,camera)
+}
+animate()
