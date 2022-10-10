53c53ac8565c5ad979c2ff0720a29636d6cbc50c
Lepka <lamaandsp@gmail.com>
2022-10-10 14:48:38 +0200

    box update

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ import * as THREE from 'three'
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5
+camera.position.z = 3
 
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
@@ -23,10 +23,17 @@ scene.add( cube );
 var light = new THREE.AmbientLight(0x404040);
 scene.add(light);
 
+window.addEventListener("wheel", onMouseWheel)
+function onMouseWheel(){
+  const t = document.body.getBoundingClientRect().top;
+  cube.rotation.x += t * 0.0005
+  cube.rotation.y += t * 0.0005
+}
+
 function animate(){
   requestAnimationFrame(animate)
-  cube.rotation.x += 0.01
-  cube.rotation.y += 0.01
+  cube.rotation.x += 0.005
+  cube.rotation.y += 0.005
   renderer.render(scene,camera)
 }
 animate()
