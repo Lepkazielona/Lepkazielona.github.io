647372c17d2797659843dca44b6734acd2b78bc1
Lpka <lamaandsp@gmail.com>
2023-05-02 21:38:28 +0200

    update text and cube speed

--- main.js
+++ main.js
@@ -38,8 +38,8 @@ window.addEventListener("touchmove", onMouseWheel)
 window.addEventListener("wheel", onMouseWheel)
 function onMouseWheel(){
   const t = document.body.getBoundingClientRect().top;
-  cube.rotation.x += 0.05
-  cube.rotation.y += 0.05
+  cube.rotation.x += 0.1
+  cube.rotation.y += 0.1
 }
 
 function animate(){
